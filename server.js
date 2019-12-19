f5b0f0c3319835085d707a35ff6416d331ecece4
Sean Halupczynski <sean.halup@gmail.com>
2019-12-18 21:20:42 -0500

    finished setting up server.js
    code is in place. just need to comment back in routes variable & use it

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 const express = require("express");
 var exphbs = require("express-handlebars");
-
-const app = express();
+// var routes = require("./controllers/burgerController.js")
+var app = express();
 
 let PORT = process.env.PORT || 5001;
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// app.use(routes)
 
 app.listen(PORT, function(err){
     if(err) throw err;
